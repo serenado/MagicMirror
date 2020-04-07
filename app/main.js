5f92b6176a4194d2545e75e2cb3f4eebd89a05d2
Claire <clairet@mit.edu>
2020-04-07 17:04:06 -0400

    reconfigure to loop constantly, not just when a hand is recognized

--- app/main.js
+++ app/main.js
@@ -1,24 +1,49 @@
-// GAME SETUP
+// SETUP
 var cursor = new Cursor();
 var clock = new Clock();
 
 // UI SETUP
 setupUserInterface();
 
-// MAIN GAME LOOP
-// Called every time the Leap provides a new frame of data
-Leap.loop({ hand: function(hand) {
+// MAIN LOOP
+Leap.loop({ enableGestures: true},  function(frame) {
   clock.update();
 
-  // Use the hand data to control the cursor's screen position
-  cursorPosition = hand.screenPosition();
-  cursorPosition[1] += 400;
-  cursor.setScreenPosition(cursorPosition);
+  // Check if hand is active
+  if (frame.hands.length > 0) {
+    hand = frame.hands[0]
+    
+    // Use the hand data to control the cursor's screen position
+    cursorPosition = hand.screenPosition();
+    cursorPosition[1] += 400;
+    cursor.setScreenPosition(cursorPosition);
 
-  // Insert code for hand gestures here
+    // GESTURE RECOGNITION
+    // Insert code for hand gestures here
 
-}}).use('screenPosition', {scale: LEAPSCALE});
+    // Testing the Leap API's built in gesture recognition
+    if(frame.valid && frame.gestures.length > 0) {
+      frame.gestures.forEach(function(gesture){
+          switch (gesture.type){
+            case "circle":
+                console.log("Circle Gesture");
+                break;
+            case "keyTap":
+                console.log("Key Tap Gesture");
+                break;
+            case "screenTap":
+                console.log("Screen Tap Gesture");
+                break;
+            case "swipe":
+                console.log("Swipe Gesture");
+                break;
+          }
+      });
+    }
+  }
+}).use('screenPosition', {scale: LEAPSCALE});
 
+// SPEECH RECOGNITION
 // processSpeech(transcript)
 //  Is called anytime speech is recognized by the Web Speech API
 // Input: 
